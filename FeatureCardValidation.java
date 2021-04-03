17c20dd59284dba24a16f31c646b3cd7f4057a78
Carl <335563110@gapps.yrdsb.ca>
2021-04-02 20:28:40 -0400

    Feature Complete

--- FeatureCardValidation.java
+++ FeatureCardValidation.java
@@ -19,11 +19,12 @@ class FeatureCardValidation {
      * Ensures that the credit/debit card number is valid
      */
     public static boolean validateCreditCard(String cardNum){
-        String algorithmInput = cardNum.replaceAll(" ", ""); // Removes spaces in the card number
+        String stringCardInput = cardNum.replaceAll(" ", ""); // Removes spaces in the card number
 
-        boolean numbersOnly = checkNumsOnly(algorithmInput); // Checks if String only contains numbers and has 9+ characters
+        boolean numbersOnly = checkNumsOnly(stringCardInput); // Checks if String only contains numbers and has 9+ characters
+        boolean luhnAlgorithmTest = luhnAlgorithm(stringCardInput);
 
-        if (numbersOnly){
+        if (numbersOnly && luhnAlgorithmTest){
             return true;
         }
         else{
@@ -36,7 +37,7 @@ class FeatureCardValidation {
         int length = input.length();
         int counter = 0;
 
-        if (length >= 9){
+        if (length >= 9){ // Ensures that the number of characters is 9 or greater
             for (int i = 0; i < 10; i++){
                 String numCheck = numbers.substring(i,i+1); // Takes the number located at index i from String numbers
                 
@@ -58,6 +59,95 @@ class FeatureCardValidation {
         }
         else{
             return false; // False returned because card has less than 9 digits
+        }
+    }
+
+    public static boolean luhnAlgorithm (String cardNum){
+        String reverse = reverseOrder(cardNum);
+        int sum1 = algorithm1(reverse);
+        int sum2 = algorithm2(reverse);
+        int grandSum = sum1 + sum2;
+
+        boolean pass = algorithm3(grandSum);
+
+        if (pass){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+   
+    public static String reverseOrder (String entry){
+        int length = entry.length(); // 11
+        String reverse = "";
+        char character;
+        String add;
+
+        for (int i = length-1; i >= 0; i--){
+            character = entry.charAt(i);
+            add = Character.toString(character);
+            reverse = reverse.concat(add);
+        }
+
+        return reverse;
+    }
+
+    public static int algorithm1 (String num){
+        int length = num.length(); // 11
+        int sum = 0;
+        char character;
+
+        for (int i = 0; i < length; i+=2){
+            character = num.charAt(i);
+            sum += Character.getNumericValue(character); // Converts char (character) to int (sum)
+        }
+        
+        return sum; // 42
+    }
+
+    public static int algorithm2 (String num){
+        int length = num.length();
+        char character;
+        int sum = 0;
+        int digit;
+        String twoDigits = "";
+        
+
+        for (int i = 1; i < length; i+=2){
+            character = num.charAt(i);
+            digit = Character.getNumericValue(character); // Converts char (character) to int (digit)
+            digit *= 2;
+
+            if (digit >= 10){ // If the number contains 2(+) digits
+                twoDigits = Integer.toString(digit); // Converts int (digit) to string (twoDigits)
+
+                for (int j = 0; j < 2; j++){
+                    character = twoDigits.charAt(j);
+                    digit = Character.getNumericValue(character);
+                    sum += digit;
+                }
+            }
+            else{ // If the number contains 1 digit
+                sum += digit;
+            }
+        }
+
+        return sum; // 28
+    }
+
+    public static boolean algorithm3 (int grandSum){
+        String number1 = Integer.toString(grandSum); // Converts int (num) to string (number)
+        int length = number1.length();
+
+        char character = number1.charAt(length-1);
+        String number2 = Character.toString(character);
+
+        if (number2.equals("0")){
+            return true;
+        }
+        else{
+            return false;
         }
     }
 }
